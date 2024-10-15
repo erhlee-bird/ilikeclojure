60472c38daf36439a2caa6046381fff2e97a528a
Eric Lee <erhlee.bird@gmail.com>
2024-10-15 13:22:42 +0000

    fix: add deps.edn to have Calva identify the right REPL environment

--- src/ilikeclojure/core.clj
+++ src/ilikeclojure/core.clj
@@ -2,3 +2,18 @@
 
 (def version
   "0.1.0")
+
+(defn foo
+  ([] (foo 101))
+  ([x] (inc x)))
+
+(comment
+  ;; alt + x "Calva: Start a Project REPL..."
+  ;;
+  ;; then for each of the following forms,
+  ;; evaluate them with one of:
+  ;; - alt + x "Calva: Evaluate Current Form" (or Ctrl + Enter)
+  ;; - alt + x "Calva: Evaluate Current Enclosing Form" (or Ctrl + Shift + Enter)
+  (require '[ilikeclojure.core :as core])
+  ilikeclojure.core/version
+  (core/foo))
